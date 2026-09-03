72d9ff1a6d21255b0b0b8dbb9c4bcaefa654ad62
RicardoCarvalho777 <riicardo.carvalho@live.com>
2026-09-03 15:46:54 -0300

    Create next.config.js

---
+++ app/next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').Next.js.Config} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+}
+module.exports = nextConfig
